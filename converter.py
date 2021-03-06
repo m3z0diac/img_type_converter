9942e9e3625679ab320540687e68655000ebec48
Hamza Elansari <72104163+hamza07-w@users.noreply.github.com>
2021-03-06 23:41:41 +0100

    Update converter.py

--- converter.py
+++ converter.py
@@ -15,8 +15,6 @@ def ToType(to):
 
 	typees = ['jpg', 'jpeg', 'png', 'webp']
 
-	typees = ['jpg', 'jpeg', 'png', 'webp']
-
 	return True if to in typees else False
 
 
@@ -35,15 +33,43 @@ def isTypeTrue(link):
 	'''
 	#check if original type is not what we want to convert to
 	# I comment it because there is a bug find it if you can
-
+	'''
 	types = ['jpg', 'jpeg', 'png', 'webp']
 
 	for i in range(4):
-		if link[-3:] == types[i] or link[-4:] == types[i]:
+		if  types[i] in (link[-3:] , link[-4:]):
 			break
 			return True
-	'''
+
 	return True
+
+def convert(url, toType):
+	
+	'''
+	the must important function that convert jpg/jpeg/png/webp to other type of images
+	'''
+
+	if isTypeTrue(url):
+
+		geturl = requests.get(url)
+		try:
+
+			with open(f'img.{toType}', 'wb') as f:
+				f.write(geturl.content)
+
+		except:
+			print("error! type not found!")
+
+		return "The picture converted seccessfuly!"
+
+	else:
+		return "error"
+
+site = input("image url: ")
+to_type = input("to jpg/jpeg/png/webp/gif : ")
+if ToType(to_type):
+	print(convert(site, to_type))
+
 
 def convert(url, toType):
 	'''
